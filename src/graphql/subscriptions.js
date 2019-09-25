c3325acb4f1170c2aaf381d312d16be339687104
Yinlin Chen <ylchen@vt.edu>
2019-09-25 13:31:16 -0400

    update data schema and setup GraphQL API

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,369 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateCollection = `subscription OnCreateCollection {
+  onCreateCollection {
+    id
+    title
+    identifier
+    description
+    creator
+    source
+    circa
+    start_date
+    end_date
+    subject
+    location
+    rights_statement
+    language
+    bibliographic_citation
+    rights_holder
+    custom_key
+    collection_category
+    visibility
+    thumbnail_path
+    parent_collection
+    create_date
+    modified_date
+    archives {
+      items {
+        id
+        title
+        identifier
+        description
+        tags
+        creator
+        source
+        circa
+        start_date
+        end_date
+        rights_statement
+        language
+        resource_type
+        belongs_to
+        location
+        medium
+        bibliographic_citation
+        rights_holder
+        format
+        related_url
+        contributor
+        custom_key
+        parent_collection
+        item_category
+        visibility
+        thumbnail_path
+        manifest_url
+        create_date
+        modified_date
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onUpdateCollection = `subscription OnUpdateCollection {
+  onUpdateCollection {
+    id
+    title
+    identifier
+    description
+    creator
+    source
+    circa
+    start_date
+    end_date
+    subject
+    location
+    rights_statement
+    language
+    bibliographic_citation
+    rights_holder
+    custom_key
+    collection_category
+    visibility
+    thumbnail_path
+    parent_collection
+    create_date
+    modified_date
+    archives {
+      items {
+        id
+        title
+        identifier
+        description
+        tags
+        creator
+        source
+        circa
+        start_date
+        end_date
+        rights_statement
+        language
+        resource_type
+        belongs_to
+        location
+        medium
+        bibliographic_citation
+        rights_holder
+        format
+        related_url
+        contributor
+        custom_key
+        parent_collection
+        item_category
+        visibility
+        thumbnail_path
+        manifest_url
+        create_date
+        modified_date
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onDeleteCollection = `subscription OnDeleteCollection {
+  onDeleteCollection {
+    id
+    title
+    identifier
+    description
+    creator
+    source
+    circa
+    start_date
+    end_date
+    subject
+    location
+    rights_statement
+    language
+    bibliographic_citation
+    rights_holder
+    custom_key
+    collection_category
+    visibility
+    thumbnail_path
+    parent_collection
+    create_date
+    modified_date
+    archives {
+      items {
+        id
+        title
+        identifier
+        description
+        tags
+        creator
+        source
+        circa
+        start_date
+        end_date
+        rights_statement
+        language
+        resource_type
+        belongs_to
+        location
+        medium
+        bibliographic_citation
+        rights_holder
+        format
+        related_url
+        contributor
+        custom_key
+        parent_collection
+        item_category
+        visibility
+        thumbnail_path
+        manifest_url
+        create_date
+        modified_date
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onCreateArchive = `subscription OnCreateArchive {
+  onCreateArchive {
+    id
+    title
+    identifier
+    description
+    tags
+    creator
+    source
+    circa
+    start_date
+    end_date
+    rights_statement
+    language
+    resource_type
+    belongs_to
+    location
+    medium
+    bibliographic_citation
+    rights_holder
+    format
+    related_url
+    contributor
+    custom_key
+    parent_collection
+    item_category
+    visibility
+    thumbnail_path
+    manifest_url
+    create_date
+    modified_date
+    collection {
+      id
+      title
+      identifier
+      description
+      creator
+      source
+      circa
+      start_date
+      end_date
+      subject
+      location
+      rights_statement
+      language
+      bibliographic_citation
+      rights_holder
+      custom_key
+      collection_category
+      visibility
+      thumbnail_path
+      parent_collection
+      create_date
+      modified_date
+      archives {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const onUpdateArchive = `subscription OnUpdateArchive {
+  onUpdateArchive {
+    id
+    title
+    identifier
+    description
+    tags
+    creator
+    source
+    circa
+    start_date
+    end_date
+    rights_statement
+    language
+    resource_type
+    belongs_to
+    location
+    medium
+    bibliographic_citation
+    rights_holder
+    format
+    related_url
+    contributor
+    custom_key
+    parent_collection
+    item_category
+    visibility
+    thumbnail_path
+    manifest_url
+    create_date
+    modified_date
+    collection {
+      id
+      title
+      identifier
+      description
+      creator
+      source
+      circa
+      start_date
+      end_date
+      subject
+      location
+      rights_statement
+      language
+      bibliographic_citation
+      rights_holder
+      custom_key
+      collection_category
+      visibility
+      thumbnail_path
+      parent_collection
+      create_date
+      modified_date
+      archives {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const onDeleteArchive = `subscription OnDeleteArchive {
+  onDeleteArchive {
+    id
+    title
+    identifier
+    description
+    tags
+    creator
+    source
+    circa
+    start_date
+    end_date
+    rights_statement
+    language
+    resource_type
+    belongs_to
+    location
+    medium
+    bibliographic_citation
+    rights_holder
+    format
+    related_url
+    contributor
+    custom_key
+    parent_collection
+    item_category
+    visibility
+    thumbnail_path
+    manifest_url
+    create_date
+    modified_date
+    collection {
+      id
+      title
+      identifier
+      description
+      creator
+      source
+      circa
+      start_date
+      end_date
+      subject
+      location
+      rights_statement
+      language
+      bibliographic_citation
+      rights_holder
+      custom_key
+      collection_category
+      visibility
+      thumbnail_path
+      parent_collection
+      create_date
+      modified_date
+      archives {
+        nextToken
+      }
+    }
+  }
+}
+`;
