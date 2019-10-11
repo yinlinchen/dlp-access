bb206e87ff6eba06f5b6d29397356b3ff7dad557
Lee Hunter <1202435+whunter@users.noreply.github.com>
2019-10-11 11:12:37 -0400

    Add carousel and contact section to homepage (#10)
    * add default carousel
    
    still on carousel
    
    add contact section and style
    
    * no local
    
    * apply contact to all pages and restyle

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
+import FeaturedCarousel from "./home/FeaturedCarousel";
+
+import "./HomePage.css";
 
 const HomePage = () => (
-    <>
-        <div id="content-wrapper" class="container" role="main">
-            <div class="row home-content">
-                <h3>A visual exhibit of selected items from the International 
-                    Archive of Women in Architecture, a joint partnership between 
-                    the College of Architecture and Urban Studies and the University 
-                    Libraries at Virginia Tech</h3>
-            </div>
-        </div>
-   </>
+  <>
+    <div className="container">
+      <FeaturedCarousel />
+      <div className="row home-content home-statement">
+        A visual exhibit of selected items from the International Archive of
+        Women in Architecture, a joint partnership between the College of
+        Architecture and Urban Studies and the University Libraries at Virginia
+        Tech
+      </div>
+    </div>
+  </>
 );
 
 export default HomePage;
