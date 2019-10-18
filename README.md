53a658d8f1c0caa55a24b9515b78ee2a7adbfad3
Yinlin Chen <ylchen@vt.edu>
2019-10-18 10:42:20 -0400

    Dev to PPRD (#13)
    * enable search and add examples
    
    * add custom query
    
    * update config
    
    * update appsync key expired time
    
    * add sample test
    
    * add Cypress package
    
    * update readme

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
 # IAWA version 2
 
-# Data Model
-* [Collection & Archive data schema](docs/data_model.md)
 
-# AppSync
-* GraphQL API: collectionarchives
-* [Query examples](docs/appsync.md)
+## Design
+* Data Model
+  * [Collection & Archive data schema](docs/data_model.md)
 
-# Environments
+* AppSync
+  * GraphQL API: collectionarchives
+  * [Query examples](docs/appsync.md)
+
+## Deployment
+* Prerequisites
+```
+npm install -g @aws-amplify/cli
+brew install yarn
+```
+
+* Environments
 
 | Environment | Development | Pre-Production | Production |
 |:-----------:|:-----------:|:--------------:|------------|
 | GitHub      |     dev     |      pprd      |    master  |
 | Amplify     |     dev     |      pprd      |    master  |
 
-# Documents
+## Running the tests
+* End-to-end testing framework [Cypress.io](https://www.cypress.io/)
+  * Start local server using ```npm start```
+  * Launch the Cypress app ```yarn run cypress open```
+
+## Documents
 * [Development workflow](https://aws-amplify.github.io/docs/cli-toolchain/quickstart#concepts-1)
 * [Design documents](docs/)
--- src/pages/ItemsPage.js
+++ src/pages/ItemsPage.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { Component } from "react";
+import { graphqlOperation } from "aws-amplify";
+import { Connect } from "aws-amplify-react";
 
-const ItemsPage = () => (
-    <>
-        <h1>Items List</h1>
-   </>
-);
+const GetCollection = `query getCollection($id: ID!) {
+    getCollection(id: $id) {
+      id
+      title
+      thumbnail_path
+    }
+  }
+  `;
 
-export default ItemsPage;
+class CollectionsPage extends Component {
+  render() {
+    const ListView = ({ collection }) => (
+      <div>
+        <h3>Single Archive</h3>
+        <ul>
+          {
+            <li key={collection.id}>
+              <img src={collection.thumbnail_path} alt={collection.title} />
+              {collection.title}
+            </li>
+          }
+        </ul>
+      </div>
+    );
+
+    return (
+      <div>
+        <Connect
+          query={graphqlOperation(GetCollection, {
+            id: "1c5216df-ad4b-4ddb-9299-5692c986f7e3"
+          })}
+        >
+          {({ data: { getCollection }, loading, errors }) => {
+            if (!(errors === undefined || errors.length === 0))
+              return <h3>Error</h3>;
+            if (loading || !getCollection) return <h3>Loading...</h3>;
+            console.log(getCollection);
+            return <ListView collection={getCollection} />;
+          }}
+        </Connect>
+      </div>
+    );
+  }
+}
+
+export default CollectionsPage;
