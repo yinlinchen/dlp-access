20524e78c1972d061933310dcfc91e48b7ab2847
Andrea Waldren <awaldren@vt.edu>
2020-02-11 10:28:12 -0500

    Added function and classes for masonry view, and reformatted ItemsList.js component. (#54)

--- src/pages/search/ItemsList.js
+++ src/pages/search/ItemsList.js
@@ -1,35 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import { ArchiveListView } from "./ArchiveListView";
 import { CollectionListView } from "./CollectionListView";
 import { GalleryView } from "./GalleryView";
 import { MasonryView } from "./MasonryView";
 
-export const ItemsList = ({
-  items = [],
-  dataType = "archive",
-  view = "List"
-}) => {
-  return (
-    <div className="search-results">
-      <div className="row justify-content-center">
-        {items.map(item => {
-          if (view === "Gallery") {
-            return (
-              <GalleryView key={item.id} item={item} dataType={dataType} />
-            );
-          } else if (view === "Masonry") {
-            return (
-              <MasonryView key={item.id} item={item} dataType={dataType} />
-            );
-          } else {
-            if (dataType === "collection") {
-              return <CollectionListView key={item.id} collection={item} />;
+class ItemsList extends Component {
+  getClassName() {
+    if (this.props.view === "Masonry") {
+      return "card-columns";
+    } else {
+      return "row justify-content-center";
+    }
+  }
+
+  render() {
+    return (
+      <div className="search-results">
+        <div className={this.getClassName()}>
+          {this.props.items.map(item => {
+            if (this.props.view === "Gallery") {
+              return (
+                <GalleryView
+                  key={item.id}
+                  item={item}
+                  dataType={this.props.dataType}
+                />
+              );
+            } else if (this.props.view === "Masonry") {
+              return (
+                <MasonryView
+                  key={item.id}
+                  item={item}
+                  dataType={this.props.dataType}
+                />
+              );
             } else {
-              return <ArchiveListView key={item.id} archive={item} />;
+              if (this.props.dataType === "collection") {
+                return <CollectionListView key={item.id} collection={item} />;
+              } else {
+                return <ArchiveListView key={item.id} archive={item} />;
+              }
             }
-          }
-        })}
+          })}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export default ItemsList;
