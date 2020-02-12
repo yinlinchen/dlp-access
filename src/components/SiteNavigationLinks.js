7eb2b5efdea39d0a261a0fac1364f953661fe20e
Lee Hunter <1202435+whunter@users.noreply.github.com>
2020-02-12 10:28:12 -0500

    change tabindex to tabIndex. change item link to archive. fix breadcrumbs bug where they were getting reversed (#56)

--- src/components/SiteNavigationLinks.js
+++ src/components/SiteNavigationLinks.js
@@ -6,35 +6,35 @@ class SiteNavigationLinks extends Component {
       <ul id="vt_main_nav" role="presentation" aria-label="Pages in Site">
         <li className="nav-item">
           <div className="link-wrapper">
-            <a href="/" tabindex="-1">
+            <a href="/" tabIndex="-1">
               Home
             </a>
           </div>
         </li>
         <li className="nav-item">
           <div className="link-wrapper">
-            <a href="/about" tabindex="-1">
+            <a href="/about" tabIndex="-1">
               About
             </a>
           </div>
         </li>
         <li className="nav-item">
           <div className="link-wrapper">
-            <a href="/terms" tabindex="-1">
+            <a href="/terms" tabIndex="-1">
               PERMISSION PAGE
             </a>
           </div>
         </li>
         <li className="nav-item">
           <div className="link-wrapper">
-            <a href="/collections" tabindex="-1">
+            <a href="/collections" tabIndex="-1">
               BROWSE COLLECTIONS
             </a>
           </div>
         </li>
         <li className="nav-item">
           <div className="link-wrapper">
-            <a href="/search" tabindex="-1">
+            <a href="/search" tabIndex="-1">
               SEARCH ITEMS
             </a>
           </div>
