807148f5439c9bbc958cc9980d4f8e8447038b6f
Tingting Jiang <tvj@vt.edu>
2020-02-21 15:21:04 -0500

    LIBTD-2044: Refactor metadata field display (display field only when the value is not empty, share and reuse text formatting functions) (#60)

--- src/pages/collections/CollectionsList.js
+++ src/pages/collections/CollectionsList.js
@@ -1,118 +1,26 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
 import { Thumbnail } from "../../components/Thumbnail";
-import { arkLinkFormatted } from "../../shared/TextFormatTools";
+import { titleFormatted } from "../../shared/TextFormatTools";
+import { FormattedAttr } from "../../shared/TextFormatTools";
 import "../../css/ListPages.css";
 
 class CollectionsList extends Component {
-  descFormatted(collection) {
-    return collection.description.substring(0, 100) + "...";
-  }
-
-  dateFormatted(collection) {
-    return (
-      parseInt(collection.start_date) + "-" + parseInt(collection.end_date)
-    );
-  }
-
-  sourceLink(collection) {
-    return { __html: collection.source };
-  }
-
   render() {
     return (
       <div>
         {this.props.collections.map(collection => (
           <li key={collection.id} className="collection-entry">
-            <h4 className="collection-title">
-              <NavLink
-                to={`/collection/${arkLinkFormatted(collection.custom_key)}`}
-              >
-                {collection.title}
-              </NavLink>
-            </h4>
+            {titleFormatted(collection, "collection")}
             <span className="collection-img">
               <Thumbnail item={collection} dataType="collection" />
             </span>
             <div className="collection-details">
-              <div className="collection-detail">
-                <table>
-                  <tbody>
-                    <tr>
-                      <td className="collection-detail-key">Identifier:</td>
-                      <td className="collection-detail-value">
-                        {collection.identifier}
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-
-              <div className="collection-detail">
-                <table>
-                  <tbody>
-                    <tr>
-                      <td className="collection-detail-key">Description:</td>
-                      <td className="collection-detail-value">
-                        {this.descFormatted(collection)}
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-
-              <div className="collection-detail">
-                <table>
-                  <tbody>
-                    <tr>
-                      <td className="collection-detail-key">Date:</td>
-                      <td className="collection-detail-value">
-                        {this.dateFormatted(collection)}
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-
-              <div className="collection-detail">
-                <table>
-                  <tbody>
-                    <tr>
-                      <td className="collection-detail-key">Source:</td>
-                      <td
-                        className="collection-detail-value"
-                        dangerouslySetInnerHTML={this.sourceLink(collection)}
-                      ></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-
-              <div className="collection-detail">
-                <table>
-                  <tbody>
-                    <tr>
-                      <td className="collection-detail-key">Language:</td>
-                      <td className="collection-detail-value">
-                        {collection.language}
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-
-              <div className="collection-detail">
-                <table>
-                  <tbody>
-                    <tr>
-                      <td className="collection-detail-key">Creator:</td>
-                      <td className="collection-detail-value">
-                        {collection.creator}
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+              <FormattedAttr item={collection} attribute="identifier" />
+              <FormattedAttr item={collection} attribute="description" />
+              <FormattedAttr item={collection} attribute="date" />
+              <FormattedAttr item={collection} attribute="source" />
+              <FormattedAttr item={collection} attribute="language" />
+              <FormattedAttr item={collection} attribute="creator" />
             </div>
           </li>
         ))}
--- src/pages/search/CollectionListView.js
+++ src/pages/search/CollectionListView.js
@@ -1,83 +1,25 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { arkLinkFormatted } from "../../shared/TextFormatTools";
+import { titleFormatted } from "../../shared/TextFormatTools";
+import { FormattedAttr } from "../../shared/TextFormatTools";
 import { Thumbnail } from "../../components/Thumbnail";
 import "../../css/SearchResult.css";
-import ReactHtmlParser from "react-html-parser";
-
-const DateValue = collection => {
-  let circa_date = collection.circa ? collection.circa : "";
-  let end_date = collection["end_date"] ? " - " + collection["end_date"] : "";
-  return circa_date + collection["start_date"] + end_date;
-};
-
-const DescFormatted = collection => {
-  return collection.description.substring(0, 100) + "...";
-};
-
-const MultiValueAttr = (label, value) => {
-  if (Array.isArray(value)) {
-    return (
-      <tr>
-        <th>{label}:</th>
-        <td>{value.join(" and ")}</td>
-      </tr>
-    );
-  } else {
-    return "";
-  }
-};
 
 export const CollectionListView = ({ collection }) => {
   return (
     <div className="row search-result-wrapper">
       <div className="col-sm-12 title-wrapper">
-        <h4>
-          <NavLink
-            to={`/collection/${arkLinkFormatted(collection.custom_key)}`}
-          >
-            {collection.title}
-          </NavLink>
-        </h4>
+        {titleFormatted(collection, "collection")}
       </div>
       <div className="col-md-4 col-sm-4 my-auto">
         <Thumbnail item={collection} dataType="collection" />
       </div>
       <div className="col-md-8 col-sm-8">
-        <table className="table">
-          <tbody>
-            <tr>
-              <th>Identifier:</th>
-              <td>
-                <NavLink
-                  to={`/collection/${arkLinkFormatted(collection.custom_key)}`}
-                >
-                  {collection.identifier}
-                </NavLink>
-              </td>
-            </tr>
-            <tr>
-              <th>Description:</th>
-              <td>{DescFormatted(collection)}</td>
-            </tr>
-            <tr>
-              <th>Date:</th>
-              <td>{DateValue(collection)}</td>
-            </tr>
-            <tr>
-              <th>Source:</th>
-              <td>
-                {collection.source.map((value, i) => (
-                  <li className="list-unstyled" key={i}>
-                    {ReactHtmlParser(value)}
-                  </li>
-                ))}
-              </td>
-            </tr>
-            {MultiValueAttr("language", collection.language)}
-            {MultiValueAttr("Creator", collection.creator)}
-          </tbody>
-        </table>
+        <FormattedAttr item={collection} attribute="identifier" />
+        <FormattedAttr item={collection} attribute="description" />
+        <FormattedAttr item={collection} attribute="date" />
+        <FormattedAttr item={collection} attribute="source" />
+        <FormattedAttr item={collection} attribute="language" />
+        <FormattedAttr item={collection} attribute="creator" />
       </div>
     </div>
   );
