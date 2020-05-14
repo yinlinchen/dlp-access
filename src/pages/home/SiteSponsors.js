2f96ef82455721633eb61ed312142092fe526d42
Andrea Waldren <awaldren@vt.edu>
2020-05-14 12:16:43 -0400

    Added sponsors logos, adjusted css for sponsors and media section; (#120)
    removes extra css in footer

--- src/pages/home/SiteSponsors.js
+++ src/pages/home/SiteSponsors.js
@@ -10,7 +10,7 @@ class SiteSponsors extends Component {
           <div className="home-sponsors-wrapper">
             {this.props.sponsors.map((sponsor, index) => (
               <div key={index} className="sponsor-wrapper">
-                <a href={sponsor.link}>
+                <a href={sponsor.link} target="_blank">
                   <img src={sponsor.img} alt={sponsor.alt} />
                 </a>
               </div>
