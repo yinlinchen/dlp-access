9ba145a97f10f88f4e4dadf70d64f0f1633afb05
Yinlin Chen <ylchen@vt.edu>
2021-01-22 09:53:34 -0500

    increase timeout

--- cypress/integration/admin_homepage_top_config.spec.js
+++ cypress/integration/admin_homepage_top_config.spec.js
@@ -47,16 +47,16 @@ describe("Update Homepage fields and revert", function() {
 
   it("Update Show title", () => {
     cy.get("input[value='edit']").parent().click();
-    cy.get("input[name='staticImageShowTitle']", { timeout: 8000 }).uncheck();
+    cy.get("input[name='staticImageShowTitle']", { timeout: 12000 }).uncheck();
     cy.contains("Update Config").click();
-    cy.contains("Show title: false", { timeout: 8000 }).should('be.visible');
+    cy.contains("Show title: false", { timeout: 12000 }).should('be.visible');
   })
 
   it("Change Show title back", () => {
     cy.get("input[value='edit']").parent().click();
-    cy.get("input[name='staticImageShowTitle']", { timeout: 8000 }).check();
+    cy.get("input[name='staticImageShowTitle']", { timeout: 12000 }).check();
     cy.contains("Update Config").click();
-    cy.contains("Show title: true", { timeout: 8000 }).should('be.visible');
+    cy.contains("Show title: true", { timeout: 12000 }).should('be.visible');
   })
 
   it("displays successful upload", () => {
