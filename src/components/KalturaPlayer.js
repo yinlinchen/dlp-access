ffab1de6ffa4f04bef55e4332c5a70c91af55a3e
Tingting Jiang <tvj@vt.edu>
2021-03-26 12:05:36 -0400

    LIBTD-2433: Pick Kalture video with specified entry_id (#250)

--- src/components/KalturaPlayer.js
+++ src/components/KalturaPlayer.js
@@ -3,7 +3,11 @@ import "../css/Viewer.scss";
 
 class KalturaPlayer extends Component {
   getURL() {
-    return `${this.props.manifest_url}?iframeembed=true&playerId=kaltura_player&entry_id=1_qvxfd4bn&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_x2fmman0`;
+    const entry_id = this.props.manifest_url.replace(
+      "https://video.vt.edu/media/",
+      ""
+    );
+    return `https://cdnapisec.kaltura.com/p/2375811/sp/237581100/embedIframeJs/uiconf_id/41951101/partner_id/2375811?iframeembed=true&playerId=kaltura_player&entry_id=${entry_id}&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[leadWithHTML5]=true&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_x2fmman0`;
   }
   render() {
     return (
